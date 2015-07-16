a509382b7a7a86963417dc828d6ff9b6751cb3ef
Huang Yonggang <huangyg@mars22.com>
2015-07-16 16:34:59 +0800

    create normal into some func

--- Joint.Token/client/JT.createnormal.js
+++ Joint.Token/client/JT.createnormal.js
@@ -13,6 +13,8 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+//var infra = require('./infra');
+
 var name,id,email,passphrase;
 
 rl.question("请输入姓名：\n", function(answer) {
@@ -25,67 +27,75 @@ rl.question("请输入姓名：\n", function(answer) {
 				passphrase = answer;
 				rl.close();
 				
-				var UserId = name + " (" + id + ") <" + email + ">" ;
-				
-				var publicKey,privateKey;
-				var opt = {numBits: 2048, userId: UserId, passphrase: passphrase};
-
-				console.log("正在创建密钥对，需要几十秒时间，请稍候。。。");
-
-				openpgp.generateKeyPair(opt).then(function(key) {
-					var data = new Object();
-					
-					data.id = key.key.primaryKey.fingerprint;
-					data.keytype = 2;
-					data.pubkey = key.publicKeyArmored;
-					data.createtime =  new Date().getTime();//Date.parse(key.key.primaryKey.created);
-					data.remark = "Normal Account";
-					
-					doc = yaml.safeDump(data);
-					var authorseckey = openpgp.key.readArmored(key.privateKeyArmored).keys[0];
-					
-					var item = new Object();
-					
-					//item.cod = "";
-					item.tag = "nor";
-					item.author = id;
-					item.data = data;
-					item.sigtype = 0;
-					
-					itemyaml = yaml.safeDump(item);
-					var options = {
-					  hostname: config.server.url,
-					  port: config.server.port,
-					  method: 'POST',
-					  headers: {
-						'Content-Type': 'application/x-yaml'
-					  }
-					};
-					
-					console.log("sending account to server...")
-					var req = http.request(options, function(res) {
-					  console.log('STATUS: ' + res.statusCode);
-					  console.log('HEADERS: ' + JSON.stringify(res.headers));
-					  res.setEncoding('utf8');
-					  res.on('data', function (chunk) {
-						fs.writeFile(chunk+".pub",key.publicKeyArmored,function(err){
-							if(err) throw err;
-							console.log("公钥文件 ",chunk+".pub 已保存.");
-						});
-						fs.writeFile(chunk+".sec",key.privateKeyArmored,function(err){
-							if(err) throw err;
-							console.log("私钥文件 ",chunk+".sec 已保存.");
-						});
-						console.log('BODY: ' + chunk);
-					  });
-					});
-					req.write(itemyaml);
-					req.end();
-				});
+				createNor(name,id,email,passphrase);
 			});
 		});
 	});
 });
 
+function createNor(name,id,email,passphrase){
+	var UserId = name + " (" + id + ") <" + email + ">" ;
+	
+	var publicKey,privateKey;
+	var opt = {numBits: 2048, userId: UserId, passphrase: passphrase};
 
-// node JT.createauto.js
+	console.log("正在创建密钥对，需要几十秒时间，请稍候。。。");
+
+	openpgp.generateKeyPair(opt).then(function(key) {
+		var data = new Object();
+		
+		data.id = key.key.primaryKey.fingerprint;
+		data.keytype = 2;
+		data.pubkey = key.publicKeyArmored;
+		data.createtime =  new Date().getTime();//Date.parse(key.key.primaryKey.created);
+		data.remark = "Normal Account";
+		
+		//doc = yaml.safeDump(data);
+		var authorseckey = openpgp.key.readArmored(key.privateKeyArmored).keys[0];
+		
+		var item = new Object();
+		
+		//item.cod = "";
+		item.tag = "nor";
+		item.author = id;
+		item.data = data;
+		item.sigtype = 0;
+		
+		sent(item,'POST',function(retstr){
+			fs.writeFile(retstr+".pub",key.publicKeyArmored,function(err){
+				if(err) throw err;
+				console.log("公钥文件 ",retstr+".pub 已保存.");
+			});
+			fs.writeFile(retstr+".sec",key.privateKeyArmored,function(err){
+				if(err) throw err;
+				console.log("私钥文件 ",retstr+".sec 已保存.");
+			});
+		});
+	});
+}
+
+// distribute storage
+function sent(item,method,callback){
+	var itemyaml = yaml.safeDump(item);
+	var options = {
+	  hostname: config.server.url,
+	  port: config.server.port,
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/x-yaml'
+	  }
+	};
+	
+	console.log("sending account to server...\n",options);
+	var req = http.request(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  res.setEncoding('utf8');
+	  res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+		callback(chunk);
+	  });
+	});
+	req.write(itemyaml);
+	req.end();
+}
